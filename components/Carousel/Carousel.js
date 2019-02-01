59d90d418651fc50976a89a1a243a53176103cb2
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-02-01 12:56:45 -0500

    stretch completed to the best of my abilities

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,38 +2,39 @@ class Carousel {
     constructor(item){
         this.item = item;
         
-
-            //using myData as my indexes
-        //this.myData = this.item.dataset.img;
-        //console.log(this.myData);
-
-
         let leftBtn = document.querySelector(".left-button");
         let rightBtn = document.querySelector(".right-button");
 
-        let currentImg = document.querySelector(`img[data-img="0"]`);
+        let currentImg = document.querySelector(`img[data-img="${myCounter}"]`);
         carImgs[currentImg.dataset.img].style.display = "block";
 
         leftBtn.addEventListener("click", () => this.leftSlide());
         rightBtn.addEventListener("click", () => this.rightSlide());
     }
     leftSlide(){
-        
+        if(myCounter === 0){
+            myCounter = 3
+        }
+    
         carImgs.forEach(image => image.style.display = "none");
-        carImgs[Math.floor(Math.random()*3)].style.display = "block";
+        //carImgs[Math.floor(Math.random()*3)].style.display = "block";
+        carImgs[myCounter--].style.display = "block";
     }
 
     rightSlide(){
-
+        if(myCounter === 3){
+            myCounter = 0
+        }
         carImgs.forEach(image => image.style.display = "none");
-        carImgs[Math.floor(Math.random()*3)].style.display = "block";
+        //carImgs[Math.floor(Math.random()*3)].style.display = "block";
+        carImgs[myCounter++].style.display = "block";
     }
 
 }
 
 
 
-
+let myCounter = 0;
 
 let carousel = document.querySelector(".carousel");
 let carImgs = document.querySelectorAll(".carousel img");
